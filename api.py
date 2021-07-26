ef894b5566ff2ceb85c86425ba403e2c62c0958f
Gagan Malvi <malvi@aospa.co>
2021-07-26 09:34:05 +0530

    api: Add basic devices API.
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- api.py
+++ api.py
@@ -1,11 +1,24 @@
 import flask
+from flask import jsonify, request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+data = [
+	{'codename' : 'onclite',
+	 'retail' : 'Redmi 7' },
+	{'codename' : 'lime',
+	 'retail' : 'Redmi 9T' },
+	{'codename' : 'phoenix',
+	 'retail' : 'POCO X2' } 
+]
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Basic Flask API Demo</h1><p>If you have successfully opened this page, your Flask application is running correctly!</p>"
+
+@app.route('/devices', methods=['GET'])
+def devices():
+    return jsonify(data)
 
 app.run()
